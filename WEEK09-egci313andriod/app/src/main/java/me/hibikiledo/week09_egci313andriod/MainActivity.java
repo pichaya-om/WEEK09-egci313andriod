54add4533cba9f5b012bae881070ec0dda84a7a9
6272 <pichayaporn.cho@student.mahidol.ac.th>
2016-11-07 13:35:59 +0700

    initial code

---
+++ WEEK09-egci313andriod/app/src/main/java/me/hibikiledo/week09_egci313andriod/MainActivity.java
@@ -0,0 +1,13 @@
+package me.hibikiledo.week09_egci313andriod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
